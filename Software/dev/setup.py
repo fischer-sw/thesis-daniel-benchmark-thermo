da9e69c9e7995999b7796a4c1e8205a772670177
NoObEfFecTs <37443900+NoObEfFecTs@users.noreply.github.com>
2021-07-06 12:38:32 +0200

    changed setup

--- Software/dev/setup.py
+++ Software/dev/setup.py
@@ -3,12 +3,19 @@
 import os
 import sys
 
-directorys = ['Datenbank', 'Datenbank/Modelle', 'Datenbank/Experimente', 'Ergebnisse']
 
 def setup(dirs):
 
-    base_path = os.path.join(sys.path[0], '../..')
+    base_path = os.path.join(sys.path[0], '..','..')
 
     for element in dirs:
 
-        os.mkdir(os.path.join(base_path, element))
+        path = os.path.join(base_path, element)
+        
+        if not os.path.isdir(path):
+            os.mkdir(path)
+
+
+if __name__ == "__main__":
+    directorys = ['Datenbank', 'Datenbank/Modelle', 'Datenbank/Experimente', 'Ergebnisse']
+    setup(directorys)
